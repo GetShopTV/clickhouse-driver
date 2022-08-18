bc196327aeabf51f63105aba70cc5149545b2ca6
Diamondy4 <Diamondy4@hotmail.com>
2022-08-18 16:26:16 +0300

    Prepared query quoting

--- src/Database/Clickhouse/Conversion/PreparedQuery.hs
+++ src/Database/Clickhouse/Conversion/PreparedQuery.hs
@@ -2,14 +2,15 @@ module Database.Clickhouse.Conversion.PreparedQuery where
 
 import Conduit
 import Control.Exception.Base (throw)
-import Data.Binary.Put (putByteString, putLazyByteString, runPut)
+import Data.Binary.Put (Put, execPut, putByteString, putCharUtf8, putLazyByteString)
+import Data.ByteString
 import Data.ByteString.Lazy qualified as BSL
 import Data.ByteString.Lazy.Char8 qualified as LC
+import Data.Csv qualified as CSV
 import Data.Foldable
 import Data.Kind
-import Database.Clickhouse.Conversion.FromClickhouse
-import Database.Clickhouse.Conversion.Types
 import Database.Clickhouse.Client.Types
+import Database.Clickhouse.Conversion.Types
 
 -- | Renderer of prepared query with question marks (?) placeholders in place of values.
 data PreparedRenderer :: Type
@@ -25,10 +26,23 @@ instance QueryRenderer PreparedRenderer where
 
 renderPrepared :: Foldable f => PreparedQuery -> f ClickhouseType -> Query
 renderPrepared (PreparedQuery qry) paramsFoldable =
-  let params = toList paramsFoldable
-      fragments = LC.split '?' qry
-   in Query . sourceLazy . runPut $ merge fragments params
+  Query $ (yield . execPut $ merge fragments params) .| builderToByteString
  where
+  params = toList paramsFoldable
+  fragments = LC.split '?' qry
   merge [x] [] = putLazyByteString x
-  merge (x : xs) (y : ys) = putLazyByteString x >> putByteString (fromClickhouseType y) >> merge xs ys
+  merge (x : xs) (y : ys) = putLazyByteString x >> renderPreparedClickhouseType y >> merge xs ys
   merge _ _ = throw WrongParamsCount
+
+renderPreparedClickhouseType :: ClickhouseType -> Put
+renderPreparedClickhouseType ct@(ClickString _) = quoted $ CSV.toField ct
+renderPreparedClickhouseType ct@(ClickDateTime _) = quoted $ CSV.toField ct
+renderPreparedClickhouseType ct@(ClickDate _) = quoted $ CSV.toField ct
+renderPreparedClickhouseType ct@(ClickUUID _) = quoted $ CSV.toField ct
+renderPreparedClickhouseType ct@(ClickIPv4 _) = quoted $ CSV.toField ct
+renderPreparedClickhouseType ct@(ClickIPv6 _) = quoted $ CSV.toField ct
+renderPreparedClickhouseType (ClickNullable Nothing) = putByteString "null"
+renderPreparedClickhouseType ct = putByteString $ CSV.toField ct
+
+quoted :: ByteString -> Put
+quoted bs = putCharUtf8 '\'' >> putByteString bs >> putCharUtf8 '\''
