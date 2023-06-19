c55fd14c75f8ace22d2660555b4ee730b91af13f
Diamondy4 <diamondy4@hotmail.com>
2023-06-19 12:51:11 +0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,32 +5,29 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    cassava-conduit = {
-      url = "github:domdere/cassava-conduit";
-      flake = false;
-    };
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.haskell-flake.flakeModule
       ];
-      perSystem = { self', inputs', system, config, final, lib, pkgs, ... }:
-        {
-          haskellProjects.default = {
-            basePackages = pkgs.haskell.packages.ghc926;
-            overrides = with pkgs.haskell.lib; hfinal: hprev: {
-              cassava-conduit = lib.pipe (hfinal.callCabal2nix "cassava-conduit" inputs.cassava-conduit { })
-                [ dontCheck ];
+      perSystem = {self', ...}: {
+        haskellProjects.default = {
+          settings = {
+            cassava-conduit = {
+              broken = false;
+              check = false;
             };
           };
-          packages.default = config.packages.clickhouse-driver;
         };
+        packages.default = self'.packages.clickhouse-driver;
+      };
     };
 }
